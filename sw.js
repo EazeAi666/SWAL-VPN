f063c8c3e0bbedaeff6243d23bdb108cef1376f6
EazeAi666 <cliton118@gmail.com>
2026-04-27 21:18:14 +0000

    Deploying to gh-pages from @ EazeAi666/SWAL-VPN@fd44ed4cfe9f2b68a7a01ca2f7a5689428a4ff41 🚀

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 const CACHE_NAME = 'swal-vpn-v1';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.webmanifest'
+  './',
+  'index.html',
+  'manifest.webmanifest'
 ];
 
 self.addEventListener('install', (event) => {
